74e8d4c49b0fff59ed059b22c455fe8cdc4b077d
syeddhasnainn <syedhasnainmurtaza@gmail.com>
2024-06-20 17:51:09 +0100

    fixed dict keys

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,21 +1,22 @@
 import { NextResponse } from "next/server";
 
-export async function GET(request: Request) {
+export async function POST(request: Request) {
     const API_KEY = process.env.AZURE_SUBSCRIPTION_KEY
     if (!API_KEY) {
         return NextResponse.json({ error: "API key not found" }, { status: 500 })
     }
+  
+    const { question } = await request.json()
 
-    const url = new URL(request.url)
-    const query = url.searchParams.get('q')
+  
 
-    if (!query) { 
+    if (!question) { 
         return NextResponse.json({ error: "Search can't be empty" }, { status: 400 }) 
     }
 
     const params = new URLSearchParams({
-        q: query || '',
-        count: '5',
+        q: question || '',
+        count: '2',
         mkt: 'en-us',
         safesearch: 'strict',
 
@@ -23,7 +24,13 @@ export async function GET(request: Request) {
 
     const res = await fetch(`https://api.bing.microsoft.com/v7.0/search?${params}`, { headers: { 'Ocp-Apim-Subscription-Key': API_KEY } })
     const data = await res.json()
+    .then(data => data.webPages.value)
+    .then(data => data.map((result:any)=> ({
+        title: result.name,
+        url: result.url,
+        description: result.snippet,
+    })))
 
-    return NextResponse.json({ data: data }, { status: 200 })
+    return NextResponse.json({ data }, { status: 200 })
 
 }
