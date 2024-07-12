ceb7c063a0f2d9492580bac1d1b4e2a7c5648de5
syeddhasnainn <syedhasnainmurtaza@gmail.com>
2024-07-12 01:20:08 +0100

    added streaming controller

---
+++ frontend/utils/streamingController.ts
@@ -0,0 +1,19 @@
+// streamingController.ts
+
+class StreamingController {
+    private abortController: AbortController | null = null;
+  
+    startStreaming() {
+      this.abortController = new AbortController();
+      return this.abortController.signal;
+    }
+  
+    stopStreaming() {
+      if (this.abortController) {
+        this.abortController.abort();
+        this.abortController = null;
+      }
+    }
+  }
+
+  export const streamingController = new StreamingController();
