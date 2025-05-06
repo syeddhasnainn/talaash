43f8408dceecd6d56d55aeefb071960e112f3c7f
syeddhasnainn <syedhasnainmurtaza@gmail.com>
2025-05-06 13:34:10 +0100

    deploy

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -3,7 +3,6 @@ export const dynamic = 'force-dynamic';
 
 export default async function ChatPage() {
   const chatid = crypto.randomUUID();
-  console.log('new chat page: ', chatid);
 
   return <Chat chatid={chatid} initialMessages={[]} />;
 }
