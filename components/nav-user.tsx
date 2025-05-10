886287c7f3a3cb6a9d684ca159e2f8534b8a3db3
syeddhasnainn <syedhasnainmurtaza@gmail.com>
2025-05-10 20:23:00 +0100

    hover edit

--- components/nav-user.tsx
+++ components/nav-user.tsx
@@ -3,13 +3,14 @@ import { SidebarMenu, SidebarMenuItem } from '@/components/ui/sidebar';
 import { Button } from './ui/button';
 import { Trash, User } from 'lucide-react';
 import { UserButton } from '@clerk/nextjs';
-import { useUser } from '@clerk/clerk-react';
+import { useUser, useClerk } from '@clerk/clerk-react';
 import { deleteAllChats } from '@/actions/chatActions';
 import { useQueryClient } from '@tanstack/react-query';
 import { toast } from 'sonner';
 
 export function NavUser() {
   const { user } = useUser();
+  const { openUserProfile } = useClerk();
   const queryClient = useQueryClient();
   return (
     <SidebarMenu>
@@ -23,7 +24,7 @@ export function NavUser() {
               });
               toast.success('All chats deleted');
             }}
-            className="rounded-lg w-full justify-start bg-card gap-2 border border-white/10 text-foreground hover:bg-red-600 hover:text-white 
+            className="rounded-lg w-full justify-start bg-card gap-2 border border-white/10 text-foreground hover:bg-red-600 hover:text-white
                      transition-colors"
             variant="ghost"
           >
@@ -32,25 +33,17 @@ export function NavUser() {
           </Button>
 
           <Button
-            className="rounded-lg w-full justify-start bg-card gap-2 border border-white/10 text-foreground hover:text-white 
-                     transition-colors"
+            onClick={() => {
+              openUserProfile();
+            }}
+            className="rounded-lg w-full justify-start bg-card gap-2 border border-white/10 text-foreground hover:text-white
+                     transition-colors cursor-pointer hover:bg-[var(--muted)]"
             variant="ghost"
             asChild
           >
             <div>
               <User className="h-4 w-4 shrink-0" />
               <span className="truncate">{user?.fullName}</span>
-
-              <UserButton
-                appearance={{
-                  elements: {
-                    userButtonTrigger:
-                      'absolute inset-0 w-full h-full opacity-0 cursor-pointer rounded-lg focus:opacity-25 focus:ring-2 focus:ring-offset-1 focus:ring-blue-500',
-                    userButtonAvatarBox: 'hidden',
-                    userButtonAvatarImage: 'hidden',
-                  },
-                }}
-              />
             </div>
           </Button>
         </div>
