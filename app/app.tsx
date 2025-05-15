855bbb1cea76b137e38c68c1b9b0e10258c5fce8
syeddhasnainn <syedhasnainmurtaza@gmail.com>
2025-05-15 17:57:11 +0100

    react router test-2

--- app/app.tsx
+++ app/app.tsx
@@ -1,17 +1,20 @@
 'use client';
-import { BrowserRouter, Routes, Route, Link } from 'react-router';
-import ChatPage from '@/app/chat/page';
-import ChatLayout from './chat/layout';
-import ChatIdPage from '@/app/chat/[id]/page';
+import { BrowserRouter, Routes, Route } from 'react-router';
+import ChatPage from '@/app/chat-page';
+import ChatLayout from './chat-layout';
+import ChatIdPage from '@/app/chatid-page';
 export default function App() {
-  return (
-    <BrowserRouter>
-        <Routes>
-          <Route element={<ChatLayout />}>
-            <Route path="/"  element={<ChatPage />} />
-            <Route path="/chat/:id" element={<ChatIdPage />} />
-          </Route>
-        </Routes>
-    </BrowserRouter>
-  );
+  if (typeof window !== 'undefined') {
+    return (
+      <BrowserRouter>
+          <Routes>
+            <Route element={<ChatLayout />}>
+              <Route path="/"  element={<ChatPage />} />
+              <Route path="/chat/:id" element={<ChatIdPage />} />
+            </Route>
+          </Routes> 
+      </BrowserRouter>  
+    ); 
+  }
+  return null;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
+'use client';
 import App from './app';
 
-export default async function Page() {
+export default function Page() {
   return <App />;
 }
